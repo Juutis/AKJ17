2362272452fa9f44ed54b871cfa1f9f83284d8a4
Juho Saarelainen <bradurdung@gmail.com>
2023-02-25 16:44:28 +0200

    Different level graphics

--- River_The_Game/Assets/Scripts/Environment/WorldManager.cs
+++ River_The_Game/Assets/Scripts/Environment/WorldManager.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class WorldManager : MonoBehaviour
 {
     public static WorldManager main;
+
+    [SerializeField]
+    private LevelConfig level;
+
+    [SerializeField]
+    private SpriteRenderer waterBackground;
+    [SerializeField]
+    private SpriteRenderer waterForeground;
+    [SerializeField]
+    private SpriteRenderer skyBackground;
+    [SerializeField]
+    private SpriteShapeRenderer groundBackground;
+
+    [SerializeField]
+    private ScaleToScreenSize bgScaler;
 
     private void Awake()
     {
@@ -21,4 +37,32 @@ public class WorldManager : MonoBehaviour
             return pickupContainer;
         }
     }
+
+    public void Start()
+    {
+        SetupVisuals();
+    }
+
+    private void SetupVisuals()
+    {
+        waterBackground.sprite = level.WaterBackground.Sprite;
+        waterBackground.color = level.WaterBackground.Color;
+        waterForeground.sprite = level.WaterForeground.Sprite;
+        waterForeground.color = level.WaterForeground.Color;
+        skyBackground.sprite = level.SkyBackground.Sprite;
+        skyBackground.color = level.SkyBackground.Color;
+        groundBackground.color = level.GroundBackground.Color;
+        bgScaler.ResizeSpriteToScreen();
+    }
+}
+
+[System.Serializable]
+public class WorldBackground
+{
+    [SerializeField]
+    private Sprite sprite;
+    public Sprite Sprite { get { return sprite; } }
+    [SerializeField]
+    private Color color;
+    public Color Color { get { return color; } }
 }

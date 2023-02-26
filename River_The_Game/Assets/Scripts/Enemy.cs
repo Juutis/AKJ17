a2c7566dddfd83e319816928ce35b9704e6cd816
Juutis <>
2023-02-26 14:27:50 +0200

    Fixes

--- River_The_Game/Assets/Scripts/Enemy.cs
+++ River_The_Game/Assets/Scripts/Enemy.cs
@@ -17,6 +17,7 @@ public class Enemy : MonoBehaviour
 
     public bool Alive = true;
     private EnemyMovement movement;
+    private FollowEnemyMovement movement2;
     private EnemyShooting shooting;
     private Vector3 lastPosition;
     private Vector3 velocity;
@@ -24,6 +25,7 @@ public class Enemy : MonoBehaviour
     private ParticleSystem deathEffect;
     private SpriteFlasher spriteFlasher;
     private EnemyAnimator anim;
+    private Collider2D collider;
 
     // Start is called before the first frame update
     void Start()
@@ -31,22 +33,31 @@ public class Enemy : MonoBehaviour
         renderer = GetComponentInChildren<SpriteRenderer>();
         rigidBody = GetComponent<Rigidbody2D>();
         movement = GetComponent<EnemyMovement>();
+        movement2 = GetComponent<FollowEnemyMovement>();
         shooting = GetComponent<EnemyShooting>();
         spriteFlasher = GetComponent<SpriteFlasher>();
         anim = GetComponentInChildren<EnemyAnimator>();
+        collider = GetComponent<Collider2D>();
         Alive = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!Alive)
+        {
+            renderer.color = Color.white;
+        }
         if (!Alive && velocity.y < 0)
         {
             var targetDir = rigidBody.velocity;
             var diff = Vector2.SignedAngle(-transform.right, targetDir);
-            var rotateSpeed = 720.0f;
-            var rotation = Mathf.Min(rotateSpeed * Time.deltaTime, Mathf.Abs(diff));
-            transform.Rotate(Vector3.forward, Mathf.Sign(diff) * rotation);
+            if (Mathf.Abs(diff) > 10.0f)
+            {
+                var rotateSpeed = 720.0f;
+                var rotation = Mathf.Min(rotateSpeed * Time.deltaTime, Mathf.Abs(diff));
+                transform.Rotate(Vector3.forward, Mathf.Sign(diff) * rotation);
+            }
         }
     }
 
@@ -84,11 +95,16 @@ public class Enemy : MonoBehaviour
     public void Kill()
     {
         renderer.sprite = deathSprite;
+        renderer.color = Color.white;
         Alive = false;
         rigidBody.gravityScale = 0.4f;
         if (movement != null)
         {
             movement.enabled = false;
+        }
+        if (movement2 != null)
+        {
+            movement2.enabled = false;
         }
         rigidBody.velocity = velocity;
         if (deathEffect != null)
@@ -114,10 +130,26 @@ public class Enemy : MonoBehaviour
         {
             enemyGroup.EnemyKilled(this);
         }
+        gameObject.layer = LayerMask.NameToLayer("Dead Enemy");
+        if (collider != null)
+        {
+            collider.isTrigger = false;
+        }
     }
 
     public void RegisterGroup(EnemyGroup group)
     {
         enemyGroup = group;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (transform.position.y < 0.0f)
+        {
+            rigidBody.velocity = Vector2.zero;
+            rigidBody.gravityScale = 0.0f;
+            rigidBody.freezeRotation = true;
+            rigidBody.simulated = false;
+        }
     }
 }

6dbc0ee428b0fcbc074ea3affcb527a69e8365cb
Juutis <>
2023-02-26 19:14:29 +0200

    SQUID

---
+++ River_The_Game/Assets/Prefabs/DamageArea.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageArea : MonoBehaviour
+{
+    [SerializeField]
+    private float duration;
+
+    [SerializeField]
+    private float radius;
+
+    private PlayerHealth player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().GetComponent<PlayerHealth>();
+        Invoke("Die", duration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.transform.position) < radius)
+        {
+            player.TakeDamage();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}

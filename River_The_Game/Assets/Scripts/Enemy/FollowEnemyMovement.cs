17f21240aa83b166c1f13431f016f6dc318450d2
Matias Pitkänen <M2tias@Gmail.com>
2023-02-26 13:37:03 +0200

    Work on level 1

--- River_The_Game/Assets/Scripts/Enemy/FollowEnemyMovement.cs
+++ River_The_Game/Assets/Scripts/Enemy/FollowEnemyMovement.cs
@@ -12,6 +12,7 @@ public class FollowEnemyMovement : MonoBehaviour
 
     private Transform player;
     private bool movementEnabled;
+    private float xPosOfActivator = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +26,7 @@ public class FollowEnemyMovement : MonoBehaviour
     {
         if (!movementEnabled) return;
 
-        bool isInTargetXPos = transform.position.x < followPhaseXPos;
+        bool isInTargetXPos = transform.position.x < (xPosOfActivator + followPhaseXPos);
         bool isNotCloseToPlayerY = transform.position.y > player.position.y + 0.1f || transform.position.y < player.position.y - 0.1f;
         if (isInTargetXPos)
         {
@@ -35,16 +36,16 @@ public class FollowEnemyMovement : MonoBehaviour
                 float yDir = transform.position.y < player.transform.position.y ? 1 : -1;
                 yPos = transform.position.y + yMovementSpeed * yDir * Time.deltaTime;
             }
-            transform.position = new Vector3(followPhaseXPos, yPos, transform.position.z);
+            transform.position = new Vector3(xPosOfActivator + followPhaseXPos, yPos, transform.position.z);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log($"{transform.name} triggered");
         if (collision.tag == "EnemyActivator")
         {
             movementEnabled = true;
+            xPosOfActivator = collision.transform.position.x;
         }
     }
 }
